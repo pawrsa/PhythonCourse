509a45b450d21aeb2df320aba800e37b36871fcb
Parsa Maleki <100044158+pawrsa@users.noreply.github.com>
2022-04-15 14:22:11 +0430

    Delete Ex 4.py

--- Ex 4.py
+++
@@ -1,8 +0,0 @@
-x = 1
-h = (x * x * x - x * x + 2) / (3 * x * x - 2 * x)
-while (-0.0001 >= h or h >= 0.0001):
-    h = (x * x * x - x * x + 2) / (3 * x * x - 2 * x)
-    
-    x = x - h
-
-print('rishe = ',x)
